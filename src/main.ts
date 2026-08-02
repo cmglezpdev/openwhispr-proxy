747bd75e796589173a702a54ffe494d3ff7b2c7d
Carlos Manuel González Peña <cmglezpdev@gmail.com>
2026-08-01 21:21:54 -0600

    chore: add log when the app starts showing the port

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,9 @@ import { EnvService } from './config/env.service';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const env = app.get(EnvService);
+
   await app.listen(env.port);
+  console.log(`Server running on port ${env.port}`);
 }
+
 bootstrap();
